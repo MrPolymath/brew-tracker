645ef618078487ad093bf2680c1888a588bd662a
mrpolymaht <danielcarmona55@gmail.com>
2022-01-28 13:02:05 +0100

    Working version of the app

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode } from 'react'
-// import Header from './Header'
 import Footer from './Footer'
 
 type Props = {
@@ -8,7 +7,6 @@ type Props = {
 
 const Layout: React.FC<Props> = (props) => (
   <div>
-    {/* <Header /> */}
     <div className="min-h-screen bg-slate-50">{props.children}</div>
     <Footer />
   </div>
